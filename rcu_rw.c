2cf207f58cd871ea2a08dda50be0260f4121335e
lizhe <sensor1010@163.com>
2024-01-05 04:09:20 -0800

    rcu: add call_rcu() rcu_rw.c
    Signed-off-by: lizhe <sensor1010@163.com>

--- rcu_rw.c
+++ rcu_rw.c
@@ -15,6 +15,12 @@ static struct my_data __rcu *global_ptr;
 static struct task_struct *writer_thread;
 static struct task_struct *reader_thread;
 
+static void my_data_free(struct rcu_head *rcu)
+{
+	struct my_data *data = container_of(rcu, struct my_data, rcu);
+	kfree(data);	// 延迟释放旧数据
+}
+
 static int writer_thread_function(void *data)
 {
 	struct my_data *new_data;
@@ -33,6 +39,13 @@ static int writer_thread_function(void *data)
 		 * 这个宏保证了指针更新操作的正确性，特别是在多核处理器环境中
 		 */
 		rcu_assign_pointer(global_ptr, new_data);
+	
+		/*
+		 * 它仅注册了一个回调函数 (my_data_free)，my_data_free() 会在所有现有的 RCU 读侧临界区结束后才执行
+		 * call_rcu() 函数本身不会阻塞调用者
+		 */
+		if (global_ptr)
+			call_rcu(&global_ptr->rcu, my_data_free);
 	}
 
 	return 0;
