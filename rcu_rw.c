6ab24dd3bf7fc10a6ba0b6f18fdb5d8bbf60c666
lizhe <sensor1010@163.com>
2024-01-05 01:04:04 -0800

    rcu: add module init and exit for rcu_rw.c
    Signed-off-by: lizhe <sensor1010@163.com>

--- rcu_rw.c
+++ rcu_rw.c
@@ -11,3 +11,25 @@ struct my_data {
 };
 
 static struct my_data __rcu *global_ptr;
+
+static struct task_struct *writer_thread;
+static struct task_struct *reader_thread;
+
+static int __init rcu_rw_init(void)
+{
+
+	return 0;
+}
+
+static void __exit rcu_rw_exit(void)
+{
+	
+}
+
+module_init(my_module_init);
+module_exit(my_module_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("lizhe");
+
+
