0029a56cb533be95b485f830bc0346cf50215ef9
Lizhe <sensor1010@163.com>
2025-05-03 09:08:06 -0700

    watchdog_dev.c: ad virt_timeout
    Signed-off-by: Lizhe <sensor1010@163.com>

--- driver/watchdog/watchdog_dev.c
+++ driver/watchdog/watchdog_dev.c
@@ -2,8 +2,15 @@
 
 static ktime_t watchdog_next_keepalive(struct watchdog_device *wdd)
 {
+	struct watchdog_core_data *wd_data = wdd->wd_data;
+	unsigned int timeout_ms = wdd->timeout * 1000;
 	ktime_t keepalive_interval;
+	ktime_t virt_timeout;
 
+	if (watchdog_active(wdd))
+		virt_timeout = ktime_add(wd_data->last_keepalive,
+				ms_to_ktime(timeout_ms));
+	
 
 	return keepalive_interval;
 }
