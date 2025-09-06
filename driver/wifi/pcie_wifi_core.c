bc34a0957fc892d5e4aa3b5bf0f6f6586682b43b
Lizhe <sensor1010@163.com>
2025-09-06 12:10:38 -0700

    drivers: wifi: add comment for pcie_wifi_core_probe
    Signed-off-by: Lizhe <sensor1010@163.com>

--- driver/wifi/pcie_wifi_core.c
+++ driver/wifi/pcie_wifi_core.c
@@ -3,6 +3,12 @@
 static int brcm_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
 	int ret;
+	/*
+	 * this structure serves as the central data hub for Broadcom PCIe wifi devices,
+	 * comprehensively encapsulating hardware state management,firmware control,
+	 * memory mapping, communication protocols, and debugging support, while acting
+	 * as the core interface for interaction between the driver and the chip
+	 * /
 	struct brcmf_pciedev_info *devinfo;
 
 	ret = -ENOMEM;
