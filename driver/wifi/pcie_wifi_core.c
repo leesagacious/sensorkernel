5e2410f27c8b471f107654f9db7cc9b2601c7e2f
Lizhe <sensor1010@163.com>
2025-09-07 09:14:36 -0700

    core: brcmf_pciedev_info structure
    Signed-off-by: Lizhe <sensor1010@163.com>

--- driver/wifi/pcie_wifi_core.c
+++ driver/wifi/pcie_wifi_core.c
@@ -1,4 +1,8 @@
 
+struct brcmf_pciedev_info {
+	
+	struct pci_dev *pdev;
+};
 
 static int brcm_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
@@ -8,13 +12,20 @@ static int brcm_pcie_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	 * comprehensively encapsulating hardware state management,firmware control,
 	 * memory mapping, communication protocols, and debugging support, while acting
 	 * as the core interface for interaction between the driver and the chip
-	 * /
+	 */
 	struct brcmf_pciedev_info *devinfo;
 
 	ret = -ENOMEM;
 	devinfo = kzalloc(sizeof(*devinfo), GFP_KERNEL);
 	if (devinfo == NULL)
 		return ret;
+
+	/*
+	 * populate the pci_dev structre maintained by it
+	 */
+	devinfo->pdev = pdev;
+
+	pcie_bus_dev->devinfo = devinfo;
 
 	return 0;
 }
