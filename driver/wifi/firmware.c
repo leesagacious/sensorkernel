a94f587cd1222a506aef759baf38fdbdb7875ce6
Lizhe <sensor1010@163.com>
2025-10-06 09:54:37 -0700

    firmeware: Retireve PCIe setting from NVRAM data
    Signed-off-by: Lizhe <sensor1010@163.com>

---
+++ driver/wifi/firmware.c
@@ -0,0 +1,23 @@
+
+/*
+ * Specifically traverse and filter NVRAM data entries related to 
+ * PCIe configurations
+ */
+static void brcmf_fw_strip_multi_v2(struct nvram_parser *nvp, u16 domain_nr,
+			u16 bus_nr)
+{
+	u8 *nvram;
+
+	/*
+	 * What nvp->nvram points to is no longer a conglomerate containing 
+	 * configurations for all devices, but rather a collection of configuratios
+	 * required solely for the current device.
+	 * this significantly reduces the amount of data needs to be uploaded
+	 * to the NIC firmware and ensures the correctness of the configurations
+	 */
+	nvram = kzalloc(nvp->nvram_len + 1 + 3 + sizeof(u32), GFP_KERNEL);
+	if (!nvram) {
+		nvp->nvram_len = 0;
+		return;
+	}
+}	
