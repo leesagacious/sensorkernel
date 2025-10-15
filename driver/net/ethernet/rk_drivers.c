ceeaa048b46cbe388d9491e11a4ef18ca2a3f831
Lizhe <sensor1010@163.com>
2025-10-15 10:19:28 -0700

    net: dwmac-rk: add gmac base address
    Signed-off-by: Lizhe <sensor1010@163.com>

--- driver/net/ethernet/rk_drivers.c
+++ driver/net/ethernet/rk_drivers.c
@@ -15,3 +15,21 @@ static int rk_gmac_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 }
+
+/*
+ * Define chip-specific operation functinos and the GMAC 
+ * base address
+ */
+static const struct rk_gmac_ops rk3588_ops = {
+	.set_to_rgmill = rk3588_set_to_rgmii,
+	.set_to_rmii = rk3588_set_to_rmii,
+	.set_rgmii_speed = rk3588_set_gmac_speed,
+	.set_rmii_speed = rk3588_set_gmac_speed,
+	.set_clock_selection = rk3588_set_clock_selection,
+	.regs_valid = true,
+	.regs = {
+		0xfe1b0000,
+		0xfe1c0000,
+		0x0
+	},
+};
