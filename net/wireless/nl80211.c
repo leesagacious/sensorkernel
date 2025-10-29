6bc6e2c73bc453833dc4c7cce4be213df02f3329
Lizhe <sensor1010@163.com>
2025-10-29 09:26:01 -0700

    wifi: rssi: add comment for rssi notification
    Signed-off-by: Lizhe <sensor1010@163.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -3,6 +3,9 @@
  * This function servers as the bridge between the wireless driver
  * and the upper network subsystem
  * responsible for handing RSSI threshold event notifications
+ *
+ * 1. Ultimately, this event will be notified to user-space programs
+ *    via the Netlink interface
  */
 void cfg80211_cqm_rssi_notify(struct net_device *dev,
 		enum nl80211_cqm_rssi_threshold_event rssi_event,
