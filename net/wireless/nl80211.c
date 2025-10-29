c0d80a2670f6eff9838473afd996a14329d73db3
Lizhe <sensor1010@163.com>
2025-10-29 09:46:03 -0700

    wifi: rssi: comment for paramter
    Signed-off-by: Lizhe <sensor1010@163.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -6,6 +6,14 @@
  *
  * 1. Ultimately, this event will be notified to user-space programs
  *    via the Netlink interface
+ *
+ * *dev: 
+ * 	which device triggered the RSSI event (e.g: wlan0 wlan1)
+ * *rssi_event:
+ * 	RSSI threshold event type
+ * 	1. NL80211_CQM_RSSI_THRESHOLD_EVENT_LOW    	Signal strength is below the configured threshold
+ * 	2. NL80211_CQM_RSSI_THRESHOLD_EVENT_HIGH	Signal strength is above the configured threshold
+ * 	3. NL80211_CQM_RSSI_BEACON_LOSS_EVENT		Reserved value, never actually sent
  */
 void cfg80211_cqm_rssi_notify(struct net_device *dev,
 		enum nl80211_cqm_rssi_threshold_event rssi_event,
