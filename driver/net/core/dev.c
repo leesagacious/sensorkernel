3ab5a214533e4e36e6454777d694a804af256fc0
Lizhe <sensor1010@163.com>
2025-11-11 09:00:20 -0800

    core: dev: add limits time of net_rx_action
    Signed-off-by: Lizhe <sensor1010@163.com>

--- driver/net/core/dev.c
+++ driver/net/core/dev.c
@@ -3,6 +3,18 @@ static __latent_entropy void net_rx_action(void)
 {
 
 	/*
+	 * The total execution time of a single network RX softirq. include
+	 * the cumulative processing time for all network device queues,
+	 * not the processing time of an individual queue
+	 *
+	 * - Limits the execution time of the 'net_rx_action()' function
+	 * - Includes the cumulative processing time of all queued netowrk 
+	 *   devices
+	 * - Represents a global time budget, not a per-queue allocation
+	 */
+	unsigned long time_limit = jiffies +
+		usecs_to_jiffies(READ_ONCE(net_hotdata.netdev_budget_usecs));
+	/*
 	 * Read the netdev_budget field from the global struct net_hotdata
 	 * 
 	 * netdev_budge: defines the maximum number of packets that can be 
