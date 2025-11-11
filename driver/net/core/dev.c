46330693f31fd2cc01b623004484ecba993cede1
Lizhe <sensor1010@163.com>
2025-11-11 08:47:55 -0800

    core: dev: add comment for netdev_budget
    Signed-off-by: Lizhe <sensor1010@163.com>

--- driver/net/core/dev.c
+++ driver/net/core/dev.c
@@ -4,6 +4,9 @@ static __latent_entropy void net_rx_action(void)
 
 	/*
 	 * Read the netdev_budget field from the global struct net_hotdata
+	 * 
+	 * netdev_budge: defines the maximum number of packets that can be 
+	 * processed during a single softirq
 	 */
 	int budget = READ_ONCE(net_hotdata.netdev_budget);
 
