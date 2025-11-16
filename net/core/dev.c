164935a5ec78c789989d9e972a9f4fb3748a60b3
Lizhe <sensor1010@163.com>
2025-11-16 09:08:55 -0800

    core: dev: Init list and hrtimer
    Signed-off-by: Lizhe <sensor1010@163.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -17,4 +17,9 @@ void netif_napi_add_weight(struct net_device *dev, struct napi_struct *napi,
 	 */
 	if (WARN_ON(test_and_set_bit(NAPI_STATE_LISTED, &napi->state)))
 		return;
+
+	INIT_LIST_HEAD(&napi->poll_list);
+	INIT_HLIST_NODE(&napi->napi_hash_node);
+	hrtimer_init(&napi->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL_PINNED);
+	napi->timer_function = napi_watchdog;
 }	
