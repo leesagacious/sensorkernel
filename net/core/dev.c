ac45639cbea0e0643989dec33ffb343f54fb6b77
Lizhe <sensor1010@163.com>
2025-11-20 07:09:49 -0800

    core: dev: add comment for napi's state
    Signed-off-by: Lizhe <sensor1010@163.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -14,6 +14,27 @@ void netif_napi_add_weight(struct net_device *dev, struct napi_struct *napi,
 	 * NAPI_STATE_LISTED: 
 	 * 	NAPI has been added to the system's NAPI 
 	 * 	list
+	 *
+	 * enum {
+  		  NAPI_STATE_SCHED,           // bit0
+    		  NAPI_STATE_MISSED,          //    1 
+    		  NAPI_STATE_DISABLE,         //    2 
+    		  NAPI_STATE_NPSVC,           //    3 
+    		  NAPI_STATE_LISTED,          //    4 
+    	  	  NAPI_STATE_NO_BUSY_POLL,    //    5 
+    		  NAPI_STATE_IN_BUSY_POLL,    //    6 
+    		  NAPI_STATE_PREFER_BUSY_POLL,//    7
+    		  NAPI_STATE_THREADED,        //    8
+    		  NAPI_STATE_SCHED_THREADED,  //    9 
+
+		Indeed, considering the number of status bits.
+		the int type seems sufficient
+
+   		Atomic bit operations in the kernel are typically
+       		based on unsigned long.
+
+                static inline void set_bit(long nr, volatile unsigned long *addr);
+	   };
 	 */
 	if (WARN_ON(test_and_set_bit(NAPI_STATE_LISTED, &napi->state)))
 		return;
